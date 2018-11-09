3594c94318b0998cf3928895b8c373fcd740093c
zayrtun <zay.t@hotmail.com>
2018-11-09 16:09:46 +0630

    Upated user add form

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -18,7 +18,7 @@
                 <div class="card-body table-responsive p-0">
                     <table class="table table-hover">
                         <thead>
-                            
+
                         </thead>
                         <tbody>
 
@@ -48,7 +48,7 @@
                                 </td>
                             </tr>
 
-                        </tbody>    
+                        </tbody>
                     </table>
                 </div>
                 <!-- /.card-body -->
@@ -67,7 +67,39 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <div class="form-group">
+                        <input v-model="form.name" type="text" name="name" placeholder="Name"
+                           class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                        <has-error :form="form" field="name"></has-error>
+                    </div>
+
+                    <div class="form-group">
+                        <input v-model="form.email" type="email" name="email" placeholder="Email"
+                           class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                        <has-error :form="form" field="email"></has-error>
+                    </div>
+
+                    <div class="form-group">
+                        <input v-model="form.email" type="email" name="email" placeholder="Email"
+                           class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                        <has-error :form="form" field="email"></has-error>
+                    </div>
+
+                    <div class="form-group">
+                        <select name="type" v-model="form.type" id="type" class="form-control" :class="{'is-invalid': form.errors.has('type')}">
+                            <option value="">Select User Role</option>
+                            <option value="admin">Admin</option>
+                            <option value="user">Regular User</option>
+                        </select>
+                        <has-error :form="form" field="type"></has-error>
+                    </div>
+
+                    <div class="form-group">
+                        <input v-model="form.password" type="password" name="password" placeholder="Password"
+                           class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                        <has-error :form="form" field="password"></has-error>
+                    </div>
+
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
@@ -81,8 +113,21 @@
 
 <script>
 export default {
-  mounted() {
-    console.log("Users mounted.");
-  }
+    data() {
+        return {
+            form: new Form({
+                name: '',
+                email: '',
+                password: '',
+                type: '',
+                bio: '',
+                photo: ''
+            })
+        }
+    },
+
+    mounted() {
+        console.log("Users mounted.");
+    }
 };
 </script>

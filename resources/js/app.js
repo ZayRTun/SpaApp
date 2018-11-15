195b6a2c21541d2b8d49ebc4decc780bee0ca4e7
zayrtun <zay.t@hotmail.com>
2018-11-15 23:15:04 +0630

    required the vue passport components

--- resources/js/app.js
+++ resources/js/app.js
@@ -69,6 +69,22 @@ window.Fire = new Vue();
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
+// Laravel Passport Components
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue')
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue')
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue')
+);
+
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 // const files = require.context('./', true, /\.vue$/i)
